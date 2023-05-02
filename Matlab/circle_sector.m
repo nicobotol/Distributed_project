3f310a9c8e6d7c409259f20f4f4543248f31ad81
nicobotol <niccolo.andreetta@gmail.com>
2023-05-02 12:03:13 +0200

    Generating the mesh on the distributed voronoi version 2

--- Matlab/circle_sector.m
+++ Matlab/circle_sector.m
@@ -2,11 +2,21 @@ function [p_circle] = circle_sector(x_center, y_center, A, B)
 % This function reports the point of a circle
 
 step = 1e-2;
-alpha_A = atan2(A(2) - y_center, A(1) - x_center);
-alpha_B = atan2(B(2) - y_center, B(1) - x_center);
+% wrapTo2Pi is used to have the angle between 0 and 2*pi
+alpha_A = wrapTo2Pi(atan2(A(2) - y_center, A(1) - x_center)); 
+alpha_B = wrapTo2Pi(atan2(B(2) - y_center, B(1) - x_center));
 
-R = norm([x_center y_center] - A); % radius of the circle
+% A is the point at the left of the agent, and B at the right
+% take into account if the the alpha_B is smaller than alpha_A -> add 2*pi in order to have it bigger
+% NOTE: we want to go always from A to B in order to have the circular sector that include the agent
+if alpha_B < alpha_A 
+  alpha_B = alpha_B + 2*pi;
+end
 
+% compute the radius of the sensing range inside the function in order to avoid numerical issues
+R = norm([x_center y_center]' - A); % radius of the circle
+
+% build the arc
 th = alpha_A:step:alpha_B;
 p_circle(:, 1) = x_center + R*cos(th);
 p_circle(:, 2) = y_center + R*sin(th);
