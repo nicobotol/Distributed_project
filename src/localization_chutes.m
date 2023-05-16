ed8266198725a101d0604e0f3eed1da041abc616
nicobotol <niccolo.andreetta@gmail.com>
2023-05-16 11:46:23 +0200

    Implementation of the relative measurement

--- src/localization_chutes.m
+++ src/localization_chutes.m
@@ -18,11 +18,13 @@ for i = 1:n_agents
         rel_mes = (agents{j}.x_real - agents{i}.x_real) + mvnrnd(zeros(inputs_len, 1), agents{i}.eps_cov)'; % perform the relative measure as the real distance between the agents plus a noise
 
         abs_mes = agents{i}.x + rel_mes; % Project the relative meas in abs.
-        agents{i}.agents_x = [agents{i}.agents_x abs_mes]; % position of the agents j known by i
+        agents{i}.agents_x(:, j) = abs_mes; % position of the agents j known by i
 
         % Propagate the uncertainty on the relative measurement
         agents{i}.agents_P_est{j} = agents{i}.P_est + agents{i}.eps_cov;
-        
+      else % if one agent does not see another, then it assumes that the other agents is further than twice the communication range, and it also sets the covariance of the estimation to a high value 
+        agents{i}.agents_x(:, j) = agents{i}.x + 2*agents{i}.Rc*ones(state_len, 1);
+        agents{i}.agents_P_est{j} = 100*ones(state_len, state_len);    
       end
     end
   end
